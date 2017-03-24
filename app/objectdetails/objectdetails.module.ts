a0aa48b18c7893e2d038177215c235ed5edd4cb1
Ralph Gasser <ralph.gasser@pontius-software.ch>
2017-03-24 13:26:51 +0100

    - Gallery and objectdetails now use the adjusted resolver to find thumbnails / files. - Added support for 3D models to ObjectdetailsComponent

--- app/objectdetails/objectdetails.module.ts
+++ app/objectdetails/objectdetails.module.ts
@@ -4,9 +4,10 @@ import {ObjectdetailsComponent} from "./objectdetails.component";
 import {BrowserModule} from "@angular/platform-browser";
 import {AppRoutingModule} from "../app-routing.module";
 import {FlexLayoutModule} from "@angular/flex-layout";
+import {M3DLoaderModule} from "../shared/components/m3d/m3d-loader-module";
 
 @NgModule({
-    imports:      [ MaterialModule.forRoot(), FlexLayoutModule, BrowserModule, AppRoutingModule ],
+    imports:      [ MaterialModule.forRoot(), FlexLayoutModule, BrowserModule, AppRoutingModule, M3DLoaderModule ],
     declarations: [ ObjectdetailsComponent ],
     exports: [ ObjectdetailsComponent ]
 })
--- app/research/containers/query-container.module.ts
+++ app/research/containers/query-container.module.ts
@@ -5,9 +5,10 @@ import {BrowserModule} from '@angular/platform-browser';
 import {QueryContainerComponent} from "./query-container.component";
 import {ImageQueryTermModule} from "./images/image-query-term.module";
 import {AudioQueryTermModule} from "./audio/audio-query-term.module";
+import {M3DQueryTermModule} from "./m3d/m3d-query-term.module";
 
 @NgModule({
-    imports:      [ BrowserModule, FormsModule, MaterialModule.forRoot(), ImageQueryTermModule, AudioQueryTermModule ],
+    imports:      [ BrowserModule, FormsModule, MaterialModule, ImageQueryTermModule, AudioQueryTermModule, M3DQueryTermModule ],
     declarations: [ QueryContainerComponent ],
     exports:      [ QueryContainerComponent ]
 })
