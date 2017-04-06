ee87f8612636d4d5894aacee9ec2524ede112bd8
Ralph Gasser <ralph.gasser@pontius-software.ch>
2017-04-06 11:10:33 +0200

    Added a service that allows easy access to the HTML5 local storage API.

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -4,12 +4,13 @@ import {CineastAPI} from "./api/cineast-api.service";
 import {ConfigService} from "./basics/config.service";
 import {LookupModule} from "./lookup/lookup.module";
 import {ResolverService} from "./basics/resolver.service";
+import {StorageService} from "./basics/storage.service";
 
 @NgModule({
     imports:      [ LookupModule ],
     exports:      [ LookupModule ],
     declarations: [ ],
-    providers:    [ QueryService,  ConfigService, ResolverService, CineastAPI ]
+    providers:    [ QueryService, ConfigService, ResolverService, CineastAPI, StorageService ]
 })
 
 export class CoreModule { }
