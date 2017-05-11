4b2c731f56f827a2a57632a846148268046c0614
Ralph Gasser <ralph.gasser@pontius-software.ch>
2017-05-11 18:11:23 +0200

    CHANGED: Removed StorageService, which is no longer required.

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -4,14 +4,13 @@ import {CineastAPI} from "./api/cineast-api.service";
 import {ConfigService} from "./basics/config.service";
 import {LookupModule} from "./lookup/lookup.module";
 import {ResolverService} from "./basics/resolver.service";
-import {StorageService} from "./basics/storage.service";
 import {EvaluationService} from "./evaluation/evaluation.service";
 
 @NgModule({
     imports:      [ LookupModule ],
     exports:      [ LookupModule ],
     declarations: [ ],
-    providers:    [ QueryService, ConfigService, ResolverService, CineastAPI, StorageService, EvaluationService ]
+    providers:    [ QueryService, ConfigService, ResolverService, CineastAPI, EvaluationService ]
 })
 
 export class CoreModule { }
