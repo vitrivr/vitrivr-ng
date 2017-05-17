a0d3e73cf19eff28b6e86c70a13332a0af53aad0
Ralph Gasser <ralph.gasser@pontius-software.ch>
2017-05-17 14:33:07 +0200

    FIXED: Removed angular2-color-picker dependency and replaced it by ngx-color-picker

--- src/app/research/containers/images/image-query-term.module.ts
+++ src/app/research/containers/images/image-query-term.module.ts
@@ -4,9 +4,9 @@ import {BrowserModule} from '@angular/platform-browser';
 import {ImageQueryTermComponent} from "./image-query-term.component";
 import {SketchDialogComponent} from "./sketch-dialog.component";
 import {SketchModule} from "../../../shared/components/sketch/sketch.module";
-import {ColorPickerModule} from "angular2-color-picker";
 import {BinarySketchDialogComponent} from "./binary-sketch-dialog.component";
 import {MaterialModule} from "../../../material.module";
+import {ColorPickerModule} from "ngx-color-picker";
 
 @NgModule({
     imports:      [ MaterialModule, BrowserModule, FormsModule, SketchModule, ColorPickerModule],
--- src/app/research/containers/query-container.module.ts
+++ src/app/research/containers/query-container.module.ts
@@ -1,4 +1,4 @@
-import {MaterialModule} from '@angular/material';
+
 import {NgModule}      from '@angular/core';
 import {FormsModule} from "@angular/forms";
 import {BrowserModule} from '@angular/platform-browser';
@@ -6,9 +6,10 @@ import {QueryContainerComponent} from "./query-container.component";
 import {ImageQueryTermModule} from "./images/image-query-term.module";
 import {AudioQueryTermModule} from "./audio/audio-query-term.module";
 import {M3DQueryTermModule} from "./m3d/m3d-query-term.module";
+import {MaterialModule} from "../../material.module";
 
 @NgModule({
-    imports:      [ BrowserModule, FormsModule, MaterialModule, ImageQueryTermModule, AudioQueryTermModule, M3DQueryTermModule ],
+    imports:      [ MaterialModule, BrowserModule, FormsModule, ImageQueryTermModule, AudioQueryTermModule, M3DQueryTermModule ],
     declarations: [ QueryContainerComponent ],
     exports:      [ QueryContainerComponent ]
 })
