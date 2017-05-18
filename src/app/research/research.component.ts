8f95af2cc9617eabddac9d51a4984ee84c748d9a
Ralph Gasser <ralph.gasser@pontius-software.ch>
2017-05-18 12:55:34 +0200

    ADDED: Button to reset query-terms and results. CHANGED: ResearchComponent now starts with one query term

--- src/app/research/research.component.ts
+++ src/app/research/research.component.ts
@@ -1,4 +1,4 @@
-import {Component} from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {QueryService} from "../core/queries/query.service";
 import {QueryContainerInterface} from "../shared/model/queries/interfaces/query-container.interface";
 import {QueryContainer} from "../shared/model/queries/query-container.model";
@@ -9,8 +9,7 @@ import {SimilarityQuery} from "../shared/model/messages/similarity-query.model";
     selector: 'research',
     templateUrl: 'research.component.html'
 })
-
-export class ResearchComponent  {
+export class ResearchComponent implements OnInit {
     /** QueryContainer's held by the current instance of ResearchComponent. */
     public readonly containers : QueryContainerInterface[] = [];
 
@@ -21,6 +20,13 @@ export class ResearchComponent  {
     constructor(private _queryService: QueryService) { }
 
     /**
+     * Lifecycle Callback (OnInit): Adds a new QueryTermContainer.
+     */
+    public ngOnInit() {
+        this.addQueryTermContainer();
+    }
+
+    /**
      * Adds a new QueryContainer to the list of QueryContainers.
      */
     public addQueryTermContainer() {
@@ -28,11 +34,20 @@ export class ResearchComponent  {
     }
 
     /**
-     * Triggers the similarity search by packing all configured QueryContainers into a single
+     * Triggers the similarity onSearchClicked by packing all configured QueryContainers into a single
      * SimilarityQuery message, and submitting that message to the QueryService.
      */
-    public search() {
+    public onSearchClicked() {
         let query = new SimilarityQuery(this.containers);
         this._queryService.findSimilar(query);
+    }
+
+    /**
+     * Clears all results and resets query terms.
+     */
+    public onClearAllClicked() {
+        this._queryService.clear();
+        this.containers.length = 0;
+        this.addQueryTermContainer();
     }
 }
