211964fa712122122b202a667f09cbde140a4cf3
Luca Rossetto <luca.rossetto@unibas.ch>
2017-07-19 10:08:52 +0200

    slight changes to how images are displayed in details and galery

--- src/app/objectdetails/objectdetails.component.ts
+++ src/app/objectdetails/objectdetails.component.ts
@@ -149,7 +149,7 @@ export class ObjectdetailsComponent implements OnInit, OnDestroy {
      *
      */
     public onImageViewerClicked() {
-        let imagePath = this._resolver.pathToThumbnail(this._mediaobject.mediatype, this._mediaobject.objectId, this._mediaobject.objectId + "_1"); //this._resolver.pathToObject(this._mediaobject)
+        let imagePath = this._resolver.pathToObject(this._mediaobject); //this._resolver.pathToThumbnail(this._mediaobject.mediatype, this._mediaobject.objectId, this._mediaobject.objectId + "_1"); //
         let dialogRef = this._dialog.open(ImagecropComponent, {data : imagePath});
         dialogRef.afterClosed().first().subscribe((result) => {
 
