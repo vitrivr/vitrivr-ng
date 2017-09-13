1e55f0dd73df3e481925f6f7b25873736fa2fef0
Ralph Gasser <ralph.gasser@pontius-software.ch>
2017-09-13 11:47:23 +0200

    ADDED: QuickViewerComponent to quickly look at and play a media object.

--- src/app/objectdetails/objectdetails.module.ts
+++ src/app/objectdetails/objectdetails.module.ts
@@ -8,12 +8,13 @@ import {M3DLoaderModule} from "../shared/components/m3d/m3d-loader-module";
 import {MaterialModule} from "../material.module";
 import {ImagecropComponent} from "./imagecrop.component";
 import {ImageCropperModule} from "ng2-img-cropper";
+import {QuickViewerComponent} from "./quick-viewer.component";
 
 @NgModule({
     imports:      [ MaterialModule, FlexLayoutModule, BrowserModule, AppRoutingModule, M3DLoaderModule, ImageCropperModule ],
-    declarations: [ ObjectdetailsComponent, ImagecropComponent ],
-    exports: [ ObjectdetailsComponent ],
-    entryComponents: [ ImagecropComponent ]
+    declarations: [ ObjectdetailsComponent, ImagecropComponent, QuickViewerComponent ],
+    exports: [ ObjectdetailsComponent, QuickViewerComponent ],
+    entryComponents: [ ImagecropComponent, QuickViewerComponent ]
 })
 
 export class ObjectdetailsModule { }
