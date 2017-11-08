f083e9cc26ab0ce8de54e276bf3b8e8408ecf782
Ralph Gasser <rg@pontius.ch>
2017-11-08 10:55:11 +0100

    ADDED: First version of VBS submit functionality.

--- src/app/shared/components/video/advanced-media-player.component.ts
+++ src/app/shared/components/video/advanced-media-player.component.ts
@@ -3,6 +3,10 @@ import {MediaObjectScoreContainer} from "../../model/features/scores/media-objec
 import {SegmentScoreContainer} from "../../model/features/scores/segment-score-container.model";
 import {ResolverService} from "../../../core/basics/resolver.service";
 import {VgAPI} from "videogular2/core";
+import {ConfigService} from "../../../core/basics/config.service";
+import {Http, RequestOptionsArgs} from "@angular/http";
+import {MetadataLookupService} from "../../../core/lookup/metadata-lookup.service";
+import {VideoUtil} from "../../util/video.util";
 
 declare var VTTCue;
 
@@ -35,17 +39,31 @@ export class AdvancedMediaPlayerComponent {
     /** The internal VgAPI reference used to interact with the media player. */
     private _track: TextTrack;
 
+    /** The FPS value for the current video (-1 = indeterminate). */
+    private _fps: number = -1;
+
     /**
      * Default constructor.
      *
-     * @param {ResolverService} _resolver
+     * @param {ResolverService} _resolver  Injected service to resolve names of resources.
+     * @param {ConfigService} _config Injected service to access application configuration.
+     * @param {Http} _http Injected service to send XHRHttpRequests
+     * @param {MetadataLookupService} _metadata Injected service to access object metadata through the Cineast API.
      */
-    constructor(public readonly _resolver: ResolverService) {
+    constructor(public readonly _resolver: ResolverService, private readonly _config: ConfigService, private _http: Http, private _metadata: MetadataLookupService) {
+        this._metadata.first().subscribe(s => {
+            for (let metadata of s.content) {
+                if (metadata.domain === "technical" && metadata.key === "fps") {
+                    this._fps = metadata.value;
+                }
+            }
+        })
     }
 
     /**
+     * Callback that is invoked once the Vg player is ready.
      *
-     * @param api
+     * @param api VgAPI instance.
      */
     public onPlayerReady(api: VgAPI) {
         this._api = api;
@@ -64,6 +82,34 @@ export class AdvancedMediaPlayerComponent {
     }
 
     /**
+     * Submits the current playbackof the player to the VBS endpoint. The VBS endpoint expects the playback position
+     * to be submitted in frames. As there is no native way to extract the frame number from a JavaScript video player,
+     * the method uses the FPS value of the video and the current time to calculate the current frame.
+     *
+     * If the FPS value is known for the video (from the metadata), that value is used. Otherwise, a best effort
+     * estimate is calculated using the focus segment.
+     */
+    public onSubmitPressed() {
+        /* Determine necessary values. */
+        let frame = 0;
+        if (this._fps <= 0) {
+            frame = Math.floor(this._api.currentTime * VideoUtil.bestEffortFPS(this.focus));
+            console.log("FPS value could not be load from metadata. Fallback to a best effort estimate.");
+        } else {
+            frame = Math.floor(this._api.currentTime * this._fps);
+        }
+
+        /* Ask for user confirmation and submit the values. */
+        if (confirm("Are you sure you want to submit the current position (frame: " + frame + ") of video '" + this.mediaobject.objectId + "' to VBS?")) {
+            let options = <RequestOptionsArgs>{params: { video: this.mediaobject.objectId, team : this._config.configuration.vbsTeam, frame: frame}};
+            this._http.get(this._config.configuration.vbsEndpoint, options).first().subscribe((r) => {
+                /* TODO: Handle response */
+                console.log(r.toString());
+            });
+        }
+    }
+
+    /**
      * Seeks to the position of the focus segment. If that position is undefined, this method has no effect.
      */
     public seekToFocusPosition() {
@@ -77,5 +123,15 @@ export class AdvancedMediaPlayerComponent {
      */
     get track(): TextTrack {
         return this._track;
+    }
+
+    /**
+     * Returns true, if the submit (to VBS) button should be displayed and false otherwise. This depends on the configuration and
+     * the media type of the object.
+     *
+     * @return {boolean}
+     */
+    get showsubmit(): boolean {
+        return this.mediaobject.mediatype == 'VIDEO' && this._config.configuration.vbsOn && (this._config.configuration.vbsEndpoint != null);
     }
 }
