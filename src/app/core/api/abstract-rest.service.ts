52e0b32c5fd05a0e0656df7ebdf3be8010f03d8c
Ralph Gasser <rg@pontius.ch>
2017-11-08 12:07:51 +0100

    REMOVED: AbstractWebService which is superfluous at this point.

--- src/app/core/api/abstract-rest.service.ts
+++
@@ -1,7 +0,0 @@
-import {HttpClient} from "@angular/common/http";
-import {Inject} from "@angular/core";
-
-export abstract class AbstractWebService {
-
-    constructor (@Inject(HttpClient) private _http: HttpClient) {}
-}
