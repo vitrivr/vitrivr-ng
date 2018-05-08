405dc66af35c03204dd2ad965ce5e9630272a9d7
Ralph Gasser <rg@pontius.ch>
2018-05-08 16:31:57 +0200

    Added a method to replace URLs by links.

--- src/app/shared/util/html.util.ts
+++ src/app/shared/util/html.util.ts
@@ -1,9 +1,8 @@
 import {SegmentScoreContainer} from "../model/results/scores/segment-score-container.model";
 
 export class HtmlUtil {
-
     /** RegEx pattern used to test whether a string is a URL. */
-    static URL_PATTERN = new RegExp('^(https?):\\/\\/((?:[a-z0-9.-]|%[0-9A-F]{2}){3,})(?::(\\d+))?((?:\\/(?:[a-z0-9-._~!$&\'()*+,;=:@]|%[0-9A-F]{2})*)*)(?:\\?((?:[a-z0-9-._~!$&\'()*+,;=:\\/?@]|%[0-9A-F]{2})*))?(?:#((?:[a-z0-9-._~!$&\'()*+,;=:\\/?@]|%[0-9A-F]{2})*))?$','i');
+    static URL_PATTERN = new RegExp('((([A-Za-z]{3,9}:(?:\\/\\/)?)(?:[-;:&=\\+\\$,\\w]+@)?[A-Za-z0-9.-]+|(?:www.|[-;:&=\\+\\$,\\w]+@)[A-Za-z0-9.-]+)((?:\\/[\\+~%\\/.\\w-_]*)?\\??(?:[-\\+=&;%@.\\w_]*)#?(?:[\\w]*))?)'))?','ig');
 
     /**
      * Calculates a best effort FPS value from the media segment's start and end. The resulting value ony makes sense for videos.
@@ -23,5 +22,20 @@ export class HtmlUtil {
      */
     static isUrl(str: string): boolean {
         return HtmlUtil.URL_PATTERN.test(str);
+    }
+
+    /**
+     * Replaces all URL occurences in the provided string by a <a href=""></a>
+     *
+     * @param {string} str String that should be replaced.
+     * @param {target} str The target for the href (e.g. _blank).
+     * @return {boolean}
+     */
+    static replaceUrlByLink(str: string, target: string = null): string {
+        if (target) {
+            return str.replace(HtmlUtil.URL_PATTERN, "<a href='$1' target='" + target + "'>$1</a>");
+        } else {
+            return str.replace(HtmlUtil.URL_PATTERN, "<a href='$1'>$1</a>");
+        }
     }
 }
