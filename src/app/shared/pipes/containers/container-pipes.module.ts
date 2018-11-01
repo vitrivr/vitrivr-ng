6bd5c3068cc7090a686a4b79416f5b311323e501
Ralph Gasser <ralph.gasser@unibas.ch>
2018-11-01 17:45:41 +0100

    Registered new pipes with module.

--- src/app/shared/pipes/containers/container-pipes.module.ts
+++ src/app/shared/pipes/containers/container-pipes.module.ts
@@ -1,9 +1,11 @@
 import {NgModule}      from '@angular/core';
 import {OrderBySegmentPipe} from "./order-by-segment.pipe";
 import {OrderByScorePipe} from "./order-by-score.pipe";
+import {FilterObjectAllPipe} from "./filter-object-all-pipe";
+import {FilterSegmentAllPipe} from "./filter-segment-all-pipe";
 @NgModule({
     imports:      [  ],
-    declarations: [ OrderBySegmentPipe, OrderByScorePipe ],
-    exports: [ OrderBySegmentPipe, OrderByScorePipe ]
+    declarations: [ OrderBySegmentPipe, OrderByScorePipe, FilterObjectAllPipe, FilterSegmentAllPipe ],
+    exports: [ OrderBySegmentPipe, OrderByScorePipe, FilterObjectAllPipe, FilterSegmentAllPipe]
 })
 export class ContainerPipesModule { }
