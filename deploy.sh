d1b4f29b31b862f87287e1bfecdec20bc19e6f9a
root <root@Node01-Blue.maas>
2018-11-27 13:56:47 +0000

    Updated deployment script.

--- deploy.sh
+++ deploy.sh
@@ -3,48 +3,66 @@
 # Path to deployment folder (adjust)
 deploy=$1
 if [ -z ${1+x} ]; then
-    echo "Error: Please specify a deployment directory.";
+    echo "Error: Please specify a deployment directory."; >&2
     exit 1
 fi
 
 if [ ! -d $deploy ]; then
-    echo "Error: Specified deployment directory '$deploy' does not exist."
+    echo "Error: Specified deployment directory '$deploy' does not exist." >&2
     exit 1
 fi
 
 # Begin...
-echo "Deploying Vitrivr NG to $deploy..."
+echo "Deploying Vitrivr NG to $deploy"
 
 # Update project and build
-echo "Step 1: Updating build..."
-git pull
-npm install
-ng build --prod
+echo "Step 1: Updating project..."
+git pull &> deploy.log
+npm install &>> deploy.log
+
+if [ $? -ne 0 ]; then
+    echo "Step 1 failed: Please check deploy.log" >&2
+    exit 1
+fi
+
+# 
+echo "Step 2: Building project..."
+ng build --prod &>> deploy.log
+
+if [ $? -ne 0 ]; then
+    echo "Step 2 failed: Please check deploy.log" >&2
+    exit 1
+fi
 
 if [ -f $deploy/config.json ]; then
     rm dist/config.json
 fi
 
 # Remove old files
-echo "Step 2: Clean-up old deployment..."
-rm $deploy/3rdpartylicenses.txt
-rm $deploy/MaterialIcons-Regular*
-rm $deploy/favicon.ico
-rm $deploy/index.html
-rm $deploy/main.*.js
-rm $deploy/polyfills.*.js
-rm $deploy/runtime.*.js
-rm $deploy/styles.*.css
-rm $deploy/videogular.*.eot
-rm $deploy/videogular.*.svg
-rm -rf $deploy/assets
+echo "Step 3a: Clean-up old deployment..."
+rm $deploy/3rdpartylicenses.txt &>> deploy.log
+rm $deploy/MaterialIcons-Regular* &>> deploy.log
+rm $deploy/favicon.ico &>> deploy.log
+rm $deploy/index.html &>> deploy.log
+rm $deploy/main.*.js &>> deploy.log
+rm $deploy/polyfills.*.js &>> deploy.log
+rm $deploy/runtime.*.js &>> deploy.log
+rm $deploy/styles.*.css &>> deploy.log
+rm $deploy/videogular.*.eot &>> deploy.log
+rm $deploy/videogular.*.svg &>> deploy.log
+rm -rf $deploy/assets &>> deploy.log
 
 # Copy files
-echo "Step 3: Deploy..."
-mv dist/* $deploy
+echo "Step 3b: Deploy..."
+mv dist/* $deploy &>> deploy.log
+
+if [ $? -ne 0 ]; then
+    echo "Step 3 failed: Please check deploy.log" >&2
+    exit 1
+fi
 
 # Optional: Adjust permissions
-chown root:root $deploy
-chown -R root:root $deploy/assets
+chown root:root $deploy &>> deploy.log
+chown -R root:root $deploy/assets &>> deploy.log
 
-echo "Successfully deployed Vitrivr NG to $deploy."
+echo "Successfully deployed Vitrivr NG to $deploy. Check deploy.log for more information."
