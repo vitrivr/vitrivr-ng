07bea11e046e8f42aca4ac028ab1cadd18429ec7
Ralph Gasser <ralph.gasser@unibas.ch>
2018-12-28 18:33:16 +0100

    The UI provides the user with a way to load segments that have been previously labelled.

--- src/app/settings/selection/selection-management.component.ts
+++ src/app/settings/selection/selection-management.component.ts
@@ -2,6 +2,7 @@ import {ChangeDetectionStrategy, Component} from "@angular/core";
 import {SelectionService} from "../../core/selection/selection.service";
 import {Observable} from "rxjs";
 import {Tag} from "../../core/selection/tag.model";
+import {QueryService} from "../../core/queries/query.service";
 
 @Component({
     moduleId: module.id,
@@ -17,14 +18,27 @@ export class SelectionManagementComponent {
     /**
      * Constructor for SelectionManagementComponent
      *
-     * @param _selectionService
+     * @param _selectionService Reference to SelectionService (by injection).
+     * @param _queryService Reference to QueryService (by injection).
      */
-    constructor(private _selectionService: SelectionService) {
+    constructor(private _selectionService: SelectionService, private _queryService: QueryService) {
         this._observable = _selectionService.asObservable();
     }
 
     /**
+     * Invoked when a user clicks a tagged segment. Performs a lookup for that segment.
+     *
+     * @param selection The selection, usually the segment ID.
+     */
+    public onSegmentClicked(selection: string) {
+        this._queryService.lookupSegment(selection);
+    }
+
+    /**
      * Invoked when a user clicks the 'Remove tag' button. Removes the selected tag from the selected item.
+     *
+     * @param selection The selection, usually the segment ID.
+     * @param tag The tag that was clicked.
      */
     public onTagButtonClicked(selection: string, tag: Tag) {
         this._selectionService.remove(tag, selection);
