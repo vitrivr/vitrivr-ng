3dc4f25092384058241458b1f2be2e9fdc2e2b9d
Ralph Gasser <ralph.gasser@unibas.ch>
2018-12-28 18:36:41 +0100

    Fixed FilterService#clear() method.

--- src/app/core/queries/filter.service.ts
+++ src/app/core/queries/filter.service.ts
@@ -105,8 +105,9 @@ export class FilterService {
      * Clears all filters. Causes an update to be published.
      */
     public clear() {
-        this._mediatypes.forEach(v => v[1] = true);
-        this._dominant.forEach(v => v[1] = true);
+        this._mediatypes.forEach((v,k) => this._mediatypes.set(k, true));
+        this._dominant.forEach((v,k) => this._dominant.set(k, true));
+        this._threshold = 0.0;
         this.update()
     }
 
