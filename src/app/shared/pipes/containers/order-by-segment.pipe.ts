658ba8baec2903b48c7037f0de5de1b335511228
Silvan Heller <silvan.heller@unibas.ch>
2019-06-03 08:51:48 +0200

    Correctly ordering segments when loading neighboring segments

--- src/app/shared/pipes/containers/order-by-segment.pipe.ts
+++ src/app/shared/pipes/containers/order-by-segment.pipe.ts
@@ -1,5 +1,5 @@
-import {Pipe, PipeTransform} from "@angular/core";
-import {SegmentScoreContainer} from "../../model/results/scores/segment-score-container.model";
+import {Pipe, PipeTransform} from '@angular/core';
+import {SegmentScoreContainer} from '../../model/results/scores/segment-score-container.model';
 
 @Pipe({
     name: 'OrderBySegmentPipe'
@@ -14,16 +14,27 @@ export class OrderBySegmentPipe implements PipeTransform {
      * @return {Array<SegmentScoreContainer>}
      */
     public transform(array: Array<SegmentScoreContainer>, desc: boolean = true): Array<SegmentScoreContainer> {
-        if(!array || array === undefined || array.length === 0) return [];
-        console.time("UI (Sort by Segment)");
-        let results = array.sort((a: SegmentScoreContainer, b: SegmentScoreContainer) => {
+        if (!array || array.length === 0) {
+            return [];
+        }
+        console.time('UI (Sort by Segment)');
+        const results = array.sort((a: SegmentScoreContainer, b: SegmentScoreContainer) => {
+            /**
+             * This logic is ok even if a.startabs or b.startabs is 0. This is because sorting by sequence number still work.
+             */
             if (desc) {
+                if (!a.startabs && !b.startabs) {
+                    return a.sequenceNumber - b.sequenceNumber
+                }
                 return a.startabs - b.startabs;
             } else {
+                if (!a.startabs && !b.startabs) {
+                    return b.sequenceNumber - a.sequenceNumber
+                }
                 return b.startabs - a.startabs;
             }
         });
-        console.timeEnd("UI (Sort by Segment)");
+        console.timeEnd('UI (Sort by Segment)');
         return results;
     }
 }
