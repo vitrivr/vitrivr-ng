87e20076ad587bc413d99ccdbaa390ebe2da1f05
Silvan Heller <silvan.heller@unibas.ch>
2020-01-04 16:30:27 +0900

    using pipe

--- src/app/results/temporal/scored-path-object-container.model.ts
+++ src/app/results/temporal/scored-path-object-container.model.ts
@@ -1,61 +1,47 @@
 import {MediaObjectScoreContainer} from '../../shared/model/results/scores/media-object-score-container.model';
 import {ScoredPath} from './scored-path.model';
 import {SegmentScoreContainer} from '../../shared/model/results/scores/segment-score-container.model';
-import {ScoredPathSegment} from './scored-path-segment.model';
 
 /**
  * A container of ScoredPath elements which belong to the same object.
  */
 export class ScoredPathObjectContainer {
 
-  /**
-   * The best scored path, this is important, as these containers are to be sorted based on that best path
-   */
-  public readonly bestPath;
+    /**
+     * The best scored path, this is important, as these containers are to be sorted based on that best path
+     */
+    public readonly bestPath;
 
-  /**
-   * Creates a new container
-   * @param objectScoreContainer the MediaObjectScoreContainer to which all these paths belong to
-   * @param scoredPaths All the ScoredPath elements for the object
-   */
-  constructor(public readonly objectScoreContainer: MediaObjectScoreContainer,
-              public readonly scoredPaths: ScoredPath[]) {
-    if (scoredPaths.length > 1) {
-      /* the bestPath is the one with the highest score */
-      this.bestPath = scoredPaths.sort((a, b) => b.score - a.score)[0];
-    } else if (scoredPaths.length === 1) {
-      this.bestPath = scoredPaths[0];
-    } else {
-      console.log(`ScoredPath empty for ${objectScoreContainer.objectId}`);
-      this.bestPath = undefined;
+    /**
+     * Creates a new container
+     * @param objectScoreContainer the MediaObjectScoreContainer to which all these paths belong to
+     * @param scoredPaths All the ScoredPath elements for the object
+     */
+    constructor(public readonly objectScoreContainer: MediaObjectScoreContainer,
+                public readonly scoredPaths: ScoredPath[]) {
+        if (scoredPaths.length > 1) {
+            /* the bestPath is the one with the highest score */
+            this.bestPath = scoredPaths.sort((a, b) => b.score - a.score)[0];
+        } else if (scoredPaths.length === 1) {
+            this.bestPath = scoredPaths[0];
+        } else {
+            console.log(`ScoredPath empty for ${objectScoreContainer.objectId}`);
+            this.bestPath = undefined;
+        }
     }
-  }
 
-  /**
-   * Returns tuples of all segments and to which path they belong
-   */
-  get pathSegmentsOrderedDesc(): ScoredPathSegment[] {
-    const tuples = new Array<ScoredPathSegment>();
-    let mark = false;
-    this.scoredPaths.sort((a, b) => b.score - a.score).forEach(scoredPath => {
-      scoredPath.segments.forEach(segment => tuples.push(new ScoredPathSegment(segment, scoredPath, mark)));
-      mark = !mark;
-    });
-    return tuples;
-  }
+    /**
+     * Returns all segments of this object, ordered by their path score in descending order.
+     */
+    get segmentsInPathOrderedDesc(): SegmentScoreContainer[] {
+        const segments = [];
+        this.scoredPaths.sort((a, b) => b.score - a.score).forEach(scoredPath => {
+            scoredPath.segments.forEach(segment => segments.push(segment));
+        });
+        return segments;
+    }
 
-  /**
-   * Returns all segments of this object, ordered by their path score in descending order.
-   */
-  get segmentsInPathOrderedDesc(): SegmentScoreContainer[] {
-    const segments = [];
-    this.scoredPaths.sort((a, b) => b.score - a.score).forEach(scoredPath => {
-      scoredPath.segments.forEach(segment => segments.push(segment));
-    });
-    return segments;
-  }
-
-  public toString() {
-    return `${this.objectScoreContainer.objectId}::${this.bestPath}`
-  }
+    public toString() {
+        return `${this.objectScoreContainer.objectId}::${this.bestPath}`
+    }
 }
--- src/app/shared/pipes/containers/container-pipes.module.ts
+++ src/app/shared/pipes/containers/container-pipes.module.ts
@@ -5,13 +5,13 @@ import {FilterPipe} from './filter.pipe';
 import {LimitPipe} from './limit.pipe';
 import {ArrayObjectSortPipe} from './array-object-sort.pipe';
 import {SetStringSortPipe} from './set-string-sort.pipe';
-import {OrderPathByScorePipe} from '../paths/order-path-by-score.pipe';
 import {OrderObjectByBestPathScorePipe} from '../paths/order-object-by-best-path-score.pipe';
+import {FlattenPathsPipe} from '../paths/flatten-paths.pipe';
 
 @NgModule({
     imports: [],
-    declarations: [OrderBySegmentPipe, OrderPathByScorePipe, OrderObjectByBestPathScorePipe, OrderByScorePipe, FilterPipe, LimitPipe, ArrayObjectSortPipe, SetStringSortPipe],
-    exports: [OrderBySegmentPipe, OrderPathByScorePipe, OrderObjectByBestPathScorePipe, OrderByScorePipe, FilterPipe, LimitPipe, ArrayObjectSortPipe, SetStringSortPipe]
+    declarations: [OrderBySegmentPipe, FlattenPathsPipe, OrderObjectByBestPathScorePipe, OrderByScorePipe, FilterPipe, LimitPipe, ArrayObjectSortPipe, SetStringSortPipe],
+    exports: [OrderBySegmentPipe, FlattenPathsPipe, OrderObjectByBestPathScorePipe, OrderByScorePipe, FilterPipe, LimitPipe, ArrayObjectSortPipe, SetStringSortPipe]
 })
 export class ContainerPipesModule {
 }
