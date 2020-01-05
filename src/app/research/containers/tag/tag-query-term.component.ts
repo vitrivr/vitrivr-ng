13c9a26a8b32ae808e6753d7834540073f9ae59f
Loris Sauter <loris.sauter@unibas.ch>
2020-01-05 14:26:42 +0100

    Fixed TagQueryTerm: tags not being correctly removed

--- src/app/research/containers/tag/tag-query-term.component.ts
+++ src/app/research/containers/tag/tag-query-term.component.ts
@@ -1,139 +1,147 @@
-import {Component, Input, ViewChild} from "@angular/core";
-import {TagQueryTerm} from "../../../shared/model/queries/tag-query-term.model";
-import {FormControl} from "@angular/forms";
-import {EMPTY, Observable} from "rxjs";
-import {Tag} from "../../../shared/model/misc/tag.model";
-import {TagsLookupService} from "../../../core/lookup/tags-lookup.service";
-import {debounceTime, map, mergeAll, startWith} from "rxjs/operators";
-import {MatAutocompleteSelectedEvent} from "@angular/material";
+import {Component, Input} from '@angular/core';
+import {TagQueryTerm} from '../../../shared/model/queries/tag-query-term.model';
+import {FormControl} from '@angular/forms';
+import {EMPTY, Observable} from 'rxjs';
+import {Tag} from '../../../shared/model/misc/tag.model';
+import {TagsLookupService} from '../../../core/lookup/tags-lookup.service';
+import {debounceTime, map, mergeAll, startWith} from 'rxjs/operators';
+import {MatAutocompleteSelectedEvent} from '@angular/material';
 
 @Component({
-    selector: 'qt-tag',
-    templateUrl: 'tag-query-term.component.html',
-    styleUrls: ['tag-query-term.component.css']
+  selector: 'qt-tag',
+  templateUrl: 'tag-query-term.component.html',
+  styleUrls: ['tag-query-term.component.css']
 })
 export class TagQueryTermComponent {
 
-    /** The TagQueryTerm object associated with this TagQueryTermComponent. That object holds all the query settings. */
-    @Input()
-    private tagTerm: TagQueryTerm;
+  /** The TagQueryTerm object associated with this TagQueryTermComponent. That object holds all the query settings. */
+  @Input()
+  private tagTerm: TagQueryTerm;
 
-    /** List of tag fields  currently displayed. */
-    private _field: FieldGroup;
+  /**
+   * Constructor for TagQueryTermComponent
+   *
+   * @param {TagsLookupService} _tagService Service used to load tags from Cineast.
+   */
+  constructor(_tagService: TagsLookupService) {
+    this._field = new FieldGroup(_tagService);
+  }
 
-    /** List of tag fields  currently displayed. */
-    private _tags: Tag[] = [];
+  /** List of tag fields  currently displayed. */
+  private _field: FieldGroup;
 
-    /**
-     * Constructor for TagQueryTermComponent
-     *
-     * @param {TagsLookupService} _tagService Service used to load tags from Cineast.
-     */
-    constructor(_tagService: TagsLookupService) {
-        this._field = new FieldGroup(_tagService);
+  /**
+   * Getter for form control.
+   *
+   * @return {any}
+   */
+  get field() {
+    return this._field;
+  }
+
+  /** List of tag fields  currently displayed. */
+  private _tags: Tag[] = [];
+
+  /**
+   * Getter for form control.
+   *
+   * @return {any}
+   */
+  get tags() {
+    return this._tags;
+  }
+
+  /**
+   * Invoked whenever the user selects a Tag from the list.
+   *
+   * @param {MatAutocompleteSelectedEvent} event The selection event.
+   */
+  public onTagSelected(event: MatAutocompleteSelectedEvent) {
+    for (const existing of this._tags) {
+        console.log(`[TagQT.onTagSelected] existing=${JSON.stringify(existing)}, event.option.value=${JSON.stringify(event.option.value)}`);
+      if (existing.id === event.option.value) {
+      }
     }
+    this.addTag(event.option.value);
+    this.field.formControl.setValue('');
+    this.tagTerm.data = 'data:application/json;base64,' + btoa(JSON.stringify(this._tags.map(v => {
+            return v;
+    })));
+  }
 
-    /**
-     * Getter for form control.
-     *
-     * @return {any}
-     */
-    get field() {
-        return this._field;
-    }
+  /**
+   * Add the specified tag to the list of tags.
+   *
+   * @param {Tag} tag The tag that should be added.
+   */
+  public addTag(tag: Tag) {
+    this._tags.push(tag);
+  }
 
-    /**
-     * Getter for form control.
-     *
-     * @return {any}
-     */
-    get tags() {
-        return this._tags;
+  /**
+   * Removes the specified tag from the list of tags.
+   *
+   * @param {Tag} tag The tag that should be removed.
+   */
+  public removeTag(tag: Tag) {
+    const index = this._tags.indexOf(tag);
+    console.log(`[TagQT] Remove=${JSON.stringify(tag)}, tags=${JSON.stringify(this._tags)}`);
+    if (index > -1) {
+      this._tags.splice(index, 1);
     }
-
-    /**
-     * Invoked whenever the user selects a Tag from the list.
-     *
-     * @param {MatAutocompleteSelectedEvent} event The selection event.
-     */
-    public onTagSelected(event: MatAutocompleteSelectedEvent) {
-        for (let existing of this._tags) {
-            if (existing.id == event.option.value) {
-            }
-        }
-        this.addTag(event.option.value);
-        this.field.formControl.setValue("");
-        this.tagTerm.data = "data:application/json;base64," + btoa(JSON.stringify(this._tags.map(v => {return v;})));
-    }
-
-    /**
-     * Add the specified tag to the list of tags.
-     *
-     * @param {Tag} tag The tag that should be added.
-     */
-    public addTag(tag: Tag) {
-        this._tags.push(tag);
-    }
-
-    /**
-     * Removes the specified tag from the list of tags.
-     *
-     * @param {Tag} tag The tag that should be removed.
-     */
-    public removeTag(tag: Tag) {
-        let index = this._tags.indexOf(tag);
-        if (index > -1) {
-            this._tags.splice(index, 1);
-        }
-    }
+      this.tagTerm.data = 'data:application/json;base64,' + btoa(JSON.stringify(this._tags.map(v => {
+          return v;
+      })));
+    console.log(`[TagQT] Tags=${JSON.stringify(this._tags)}`);
+  }
 }
 
 /**
  * Groups the FormControl and the data source Observable of a specific tag field.
  */
 export class FieldGroup {
-    /** The FormControl used to control the tag field. */
-    public readonly formControl: FormControl = new FormControl();
+  /** The FormControl used to control the tag field. */
+  public readonly formControl: FormControl = new FormControl();
 
-    /** The Observable that acts as data source for the field. */
-    public readonly filteredTags: Observable<Tag[]>;
+  /** The Observable that acts as data source for the field. */
+  public readonly filteredTags: Observable<Tag[]>;
 
-    /** The currently selected tag. */
-    private _selection: Tag;
+  /**
+   * Constructor for FieldGroup
+   *
+   * @param {TagsLookupService} _tags
+   */
+  constructor(private _tags: TagsLookupService) {
+    this.filteredTags = this.formControl.valueChanges.pipe(
+      debounceTime(250),
+      startWith(''),
+      map((tag: string) => {
+        if (tag.length >= 3) {
+          return this._tags.matching(tag)
+        } else {
+          return EMPTY;
+        }
+      }),
+      mergeAll()
+    );
+  }
 
-    /**
-     * Constructor for FieldGroup
-     *
-     * @param {TagsLookupService} _tags
-     */
-    constructor(private _tags: TagsLookupService) {
-        this.filteredTags = this.formControl.valueChanges.pipe(
-            debounceTime(250),
-            startWith(''),
-            map((tag: string) => {
-                if (tag.length >= 3) {
-                    return this._tags.matching(tag)
-                } else {
-                    return EMPTY;
-                }
-            }),
-            mergeAll()
-        );
-    }
+  /** The currently selected tag. */
+  private _selection: Tag;
 
-    /**
-     *
-     * @return {Tag}
-     */
-    get selection(): Tag {
-        return this._selection;
-    }
+  /**
+   *
+   * @return {Tag}
+   */
+  get selection(): Tag {
+    return this._selection;
+  }
 
-    /**
-     *
-     * @param {Tag} value
-     */
-    set selection(value: Tag) {
-        this._selection = value;
-    }
+  /**
+   *
+   * @param {Tag} value
+   */
+  set selection(value: Tag) {
+    this._selection = value;
+  }
 }
--- src/app/research/research.component.ts
+++ src/app/research/research.component.ts
@@ -1,122 +1,126 @@
 import {Component, HostListener, OnInit} from '@angular/core';
-import {QueryService} from "../core/queries/query.service";
-import {QueryContainerInterface} from "../shared/model/queries/interfaces/query-container.interface";
-import {QueryContainer} from "../shared/model/queries/query-container.model";
-import {EventBusService} from "../core/basics/event-bus.service";
-import {ContextKey, InteractionEventComponent} from "../shared/model/events/interaction-event-component.model";
-import {InteractionEventType} from "../shared/model/events/interaction-event-type.model";
-import {InteractionEvent} from "../shared/model/events/interaction-event.model";
-import {from} from "rxjs";
-import {bufferCount, flatMap, map} from "rxjs/operators";
-import {FilterService} from "../core/queries/filter.service";
+import {QueryService} from '../core/queries/query.service';
+import {QueryContainerInterface} from '../shared/model/queries/interfaces/query-container.interface';
+import {QueryContainer} from '../shared/model/queries/query-container.model';
+import {EventBusService} from '../core/basics/event-bus.service';
+import {ContextKey, InteractionEventComponent} from '../shared/model/events/interaction-event-component.model';
+import {InteractionEventType} from '../shared/model/events/interaction-event-type.model';
+import {InteractionEvent} from '../shared/model/events/interaction-event.model';
+import {from} from 'rxjs';
+import {bufferCount, flatMap, map} from 'rxjs/operators';
+import {FilterService} from '../core/queries/filter.service';
 
 
 @Component({
-    moduleId: module.id,
-    selector: 'research',
-    templateUrl: 'research.component.html'
+  moduleId: module.id,
+  selector: 'research',
+  templateUrl: 'research.component.html'
 })
 export class ResearchComponent implements OnInit {
-    /** QueryContainer's held by the current instance of ResearchComponent. */
-    public readonly containers : QueryContainerInterface[] = [];
+  /** QueryContainer's held by the current instance of ResearchComponent. */
+  public readonly containers: QueryContainerInterface[] = [];
 
-    /** A timestamp used to store the timestamp of the last Enter-hit by the user. Required for shortcut detection. */
-    private _lastEnter: number = 0;
+  /** A timestamp used to store the timestamp of the last Enter-hit by the user. Required for shortcut detection. */
+  private _lastEnter: number = 0;
 
-    /**
-     * Constructor for ResearchComponent. Injects the gloal QueryService and EventBusService instance.
-     *
-     * @param _queryService QueryService instance (Singleton) used to issue queries.
-     * @param _filterService FilterService instance (Singleton) used for when user hits the clear all button.
-     * @param _eventBus EventBusService instance (Singleton) used to publish user interaction information.
-     */
-    constructor(private _queryService: QueryService, private _filterService: FilterService, private _eventBus: EventBusService) {}
+  /**
+   * Constructor for ResearchComponent. Injects the gloal QueryService and EventBusService instance.
+   *
+   * @param _queryService QueryService instance (Singleton) used to issue queries.
+   * @param _filterService FilterService instance (Singleton) used for when user hits the clear all button.
+   * @param _eventBus EventBusService instance (Singleton) used to publish user interaction information.
+   */
+  constructor(private _queryService: QueryService, private _filterService: FilterService, private _eventBus: EventBusService) {
+  }
 
-    /**
-     * Lifecycle Callback (OnInit): Adds a new QueryTermContainer.
-     */
-    public ngOnInit() {
-        this.addQueryTermContainer();
-    }
+  /**
+   * Lifecycle Callback (OnInit): Adds a new QueryTermContainer.
+   */
+  public ngOnInit() {
+    this.addQueryTermContainer();
+  }
 
-    /**
-     * Adds a new QueryContainer to the list of QueryContainers.
-     */
-    public addQueryTermContainer() {
-        this.containers.push(new QueryContainer())
-    }
+  /**
+   * Adds a new QueryContainer to the list of QueryContainers.
+   */
+  public addQueryTermContainer() {
+    this.containers.push(new QueryContainer())
+  }
 
-    /**
-     * Triggers the similarity onSearchClicked by packing all configured QueryContainers into a single
-     * SimilarityQuery message, and submitting that message to the QueryService.
-     */
-    public onSearchClicked() {
-        this._queryService.findSimilar(this.containers);
-        from(this.containers).pipe(
-            flatMap(c => c.terms),
-            map(t => {
-               let context: Map<ContextKey,any> = new Map();
-               context.set("q:categories", t.categories);
-               switch (t.type) {
-                    case "IMAGE":
-                        return new InteractionEventComponent(InteractionEventType.QUERY_IMAGE, context);
-                    case "AUDIO":
-                        return new InteractionEventComponent(InteractionEventType.QUERY_AUDIO, context);
-                    case "MOTION":
-                        return new InteractionEventComponent(InteractionEventType.QUERY_MOTION, context);
-                    case "MODEL3D":
-                        return new InteractionEventComponent(InteractionEventType.QUERY_MODEL3D, context);
-                    case "SEMANTIC":
-                       return new InteractionEventComponent(InteractionEventType.QUERY_SEMANTIC, context);
-                    case "TEXT":
-                        context.set("q:value", t.data);
-                        return new InteractionEventComponent(InteractionEventType.QUERY_FULLTEXT, context);
-                    case "TAG":
-                        context.set("q:value", t.data);
-                        return new InteractionEventComponent(InteractionEventType.QUERY_TAG, context);
-                }
-            }),
-            bufferCount(Number.MIN_SAFE_INTEGER)
-        ).subscribe(c => this._eventBus.publish(new InteractionEvent(...c)))
-    }
-
-    /**
-     * Clears all results and resets query terms.
-     */
-    public onClearAllClicked() {
-        this._queryService.clear();
-        this._filterService.clear();
-        this.containers.length = 0;
-        this.addQueryTermContainer();
-        this._eventBus.publish(new InteractionEvent(new InteractionEventComponent(InteractionEventType.CLEAR)));
-    }
-
-    /**
-     * Detects certain key combinations and takes appropriate action.
-     *
-     * @param {KeyboardEvent} event
-     */
-    @HostListener('window:keyup', ['$event'])
-    public keyEvent(event: KeyboardEvent) {
-        /** Detects a double-enter, which will trigger a new search. */
-        if (event.keyCode === 13) {
-            let timestamp = Date.now();
-            if (timestamp - this._lastEnter < 1000) {
-                this.onSearchClicked();
-                this._lastEnter = 0;
-            } else {
-                this._lastEnter = timestamp;
-            }
+  /**
+   * Triggers the similarity onSearchClicked by packing all configured QueryContainers into a single
+   * SimilarityQuery message, and submitting that message to the QueryService.
+   */
+  public onSearchClicked() {
+    this._queryService.findSimilar(this.containers);
+    from(this.containers).pipe(
+      flatMap(c => c.terms),
+      map(t => {
+          console.log(`[Research] T=${JSON.stringify(t)}`);
+        const context: Map<ContextKey, any> = new Map();
+        context.set('q:categories', t.categories);
+        switch (t.type) {
+          case 'IMAGE':
+            return new InteractionEventComponent(InteractionEventType.QUERY_IMAGE, context);
+          case 'AUDIO':
+            return new InteractionEventComponent(InteractionEventType.QUERY_AUDIO, context);
+          case 'MOTION':
+            return new InteractionEventComponent(InteractionEventType.QUERY_MOTION, context);
+          case 'MODEL3D':
+            return new InteractionEventComponent(InteractionEventType.QUERY_MODEL3D, context);
+          case 'SEMANTIC':
+            return new InteractionEventComponent(InteractionEventType.QUERY_SEMANTIC, context);
+          case 'TEXT':
+            context.set('q:value', t.data);
+            return new InteractionEventComponent(InteractionEventType.QUERY_FULLTEXT, context);
+          case 'TAG':
+              console.log(`[Research] t.data=${atob(t.data.substring('data:application/json;base64,'.length))}`)
+            context.set('q:value', t.data);
+            console.log(`[Research] context=${JSON.stringify(context)}`);
+            return new InteractionEventComponent(InteractionEventType.QUERY_TAG, context);
         }
+      }),
+      bufferCount(Number.MIN_SAFE_INTEGER)
+    ).subscribe(c => this._eventBus.publish(new InteractionEvent(...c)))
+  }
 
-        /** F1 will trigger a search. */
-        if (event.keyCode == 112) {
-            this.onSearchClicked();
-        }
+  /**
+   * Clears all results and resets query terms.
+   */
+  public onClearAllClicked() {
+    this._queryService.clear();
+    this._filterService.clear();
+    this.containers.length = 0;
+    this.addQueryTermContainer();
+    this._eventBus.publish(new InteractionEvent(new InteractionEventComponent(InteractionEventType.CLEAR)));
+  }
 
-        /** F2 will reset the search. */
-        if ( event.keyCode == 113) {
-            this.onClearAllClicked();
-        }
+  /**
+   * Detects certain key combinations and takes appropriate action.
+   *
+   * @param {KeyboardEvent} event
+   */
+  @HostListener('window:keyup', ['$event'])
+  public keyEvent(event: KeyboardEvent) {
+    /** Detects a double-enter, which will trigger a new search. */
+    if (event.keyCode === 13) {
+      let timestamp = Date.now();
+      if (timestamp - this._lastEnter < 1000) {
+        this.onSearchClicked();
+        this._lastEnter = 0;
+      } else {
+        this._lastEnter = timestamp;
+      }
     }
+
+    /** F1 will trigger a search. */
+    if (event.keyCode == 112) {
+      this.onSearchClicked();
+    }
+
+    /** F2 will reset the search. */
+    if (event.keyCode == 113) {
+      this.onClearAllClicked();
+    }
+  }
 }
