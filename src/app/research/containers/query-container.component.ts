394a7f87f41c8716c6b9f014c76808e9f03d02e1
Loris Sauter <loris.sauter@unibas.ch>
2020-01-09 12:14:31 +0100

    Added buttons (layout and style follows later) to the query containers

--- src/app/research/containers/query-container.component.ts
+++ src/app/research/containers/query-container.component.ts
@@ -1,63 +1,88 @@
-import {Component, Input} from "@angular/core";
-import {QueryContainerInterface} from "../../shared/model/queries/interfaces/query-container.interface";
-import {QueryTermType} from "../../shared/model/queries/interfaces/query-term-type.interface";
-import {ConfigService} from "../../core/basics/config.service";
-import {Config} from "../../shared/model/config/config.model";
-import {Observable} from "rxjs";
+import {Component, Input} from '@angular/core';
+import {QueryContainerInterface} from '../../shared/model/queries/interfaces/query-container.interface';
+import {QueryTermType} from '../../shared/model/queries/interfaces/query-term-type.interface';
+import {ConfigService} from '../../core/basics/config.service';
+import {Config} from '../../shared/model/config/config.model';
+import {Observable} from 'rxjs';
 
 @Component({
-    selector: 'query-container',
-    templateUrl: 'query-container.component.html'
+  selector: 'query-container',
+  templateUrl: 'query-container.component.html'
 })
 
 export class QueryContainerComponent {
-    /** The QueryContainer this QueryContainerComponent is associated to. */
-    @Input() containerModel : QueryContainerInterface;
+  /** The QueryContainer this QueryContainerComponent is associated to. */
+  @Input() containerModel: QueryContainerInterface;
 
-    /** A reference to the lists of QueryContainers (to enable removing the container). */
-    @Input() inList : QueryContainerInterface[];
+  /** A reference to the lists of QueryContainers (to enable removing the container). */
+  @Input() inList: QueryContainerInterface[];
 
-    /** A reference to the observable Config exposed by ConfigService. */
-    private _config: Observable<Config>;
+  /**
+   * Constructor; injects ConfigService
+   *
+   * @param {ConfigService} _configService
+   */
+  constructor(_configService: ConfigService) {
+    this._config = _configService.asObservable();
+  }
 
-    /**
-     * Constructor; injects ConfigService
-     *
-     * @param {ConfigService} _configService
-     */
-    constructor(_configService: ConfigService) {
-        this._config = _configService.asObservable();
+  /** A reference to the observable Config exposed by ConfigService. */
+  private _config: Observable<Config>;
+
+  /**
+   * Getter for config.
+   *
+   * @return {Config}
+   */
+  get config(): Observable<Config> {
+    return this._config;
+  }
+
+  /**
+   * Triggered, when a user clicks the remove-button (top-right corner). Removes
+   * the QueryContainerComponent from the list.
+   */
+  public onRemoveButtonClicked() {
+    const index = this.inList.indexOf(this.containerModel);
+    if (index > -1) {
+      this.inList.splice(index, 1)
     }
+  }
 
-    /**
-     * Getter for config.
-     *
-     * @return {Config}
-     */
-    get config(): Observable<Config> {
-        return this._config;
+  /**
+   *
+   * @param type
+   */
+  public onToggleButtonClicked(type: QueryTermType) {
+    if (this.containerModel.hasTerm(type)) {
+      this.containerModel.removeTerm(type);
+    } else {
+      this.containerModel.addTerm(type);
     }
+  }
 
-    /**
-     * Triggered, when a user clicks the remove-button (top-right corner). Removes
-     * the QueryContainerComponent from the list.
-     */
-    public onRemoveButtonClicked() {
-        let index = this.inList.indexOf(this.containerModel);
-        if (index > -1) {
-            this.inList.splice(index, 1)
-        }
+  /**
+   * Handler to move this query container one up (in the list of query containers)
+   */
+  public moveQueryContainerUp() {
+    console.log(`[QueryC.up] Before = ${this.inList}`)
+    const index = this.inList.indexOf(this.containerModel);
+    if (index > 0) {
+      const container = this.inList[index - 1];
+      this.inList[index - 1] = this.containerModel;
+      this.inList[index] = container;
     }
+    console.log(`[QueryC.up] After = ${this.inList}`)
+  }
 
-    /**
-     *
-     * @param type
-     */
-    public onToggleButtonClicked(type: QueryTermType) {
-         if (this.containerModel.hasTerm(type)) {
-             this.containerModel.removeTerm(type);
-         } else {
-             this.containerModel.addTerm(type);
-         }
+  public moveQueryContainerDown() {
+    console.log(`[QueryC.down] Before = ${this.inList}`)
+    const index = this.inList.indexOf(this.containerModel);
+    if (index > -1 && index < this.inList.length - 1) {
+      const container = this.inList[index + 1];
+      this.inList[index + 1] = this.containerModel;
+      this.inList[index] = container;
     }
+    console.log(`[QueryC.down] After = ${this.inList}`)
+  }
 }
