c6979870fcb5d1ace4d52f1440969d22f9619ebb
Loris Sauter <loris.sauter@unibas.ch>
2020-11-29 22:37:44 +0100

    Removed exposure of internal ids in vbs result log

--- src/app/core/vbs/vbs-results-log.model.ts
+++ src/app/core/vbs/vbs-results-log.model.ts
@@ -66,7 +66,7 @@ export class VbsResultsLog implements VbsSubmission {
     results.sortType.push(context);
     list.forEach((segmentScoreContainer, index) => {
       results.results.push(<VbsResult>{
-        video: segmentScoreContainer.objectId,
+        video: segmentScoreContainer.objectId.replace('v_', ''),
         shot: segmentScoreContainer.sequenceNumber,
         score: segmentScoreContainer.score,
         rank: index
