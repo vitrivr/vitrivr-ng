dfc6542c102126393e33489b46fad17a770ffd9f
silvanheller <silvan.heller@unibas.ch>
2022-05-15 16:10:42 +0200

    adding ability to view metadata when loading segment details

--- src/app/segmentdetails/segmentfeatures.module.ts
+++ src/app/segmentdetails/segmentfeatures.module.ts
@@ -6,12 +6,12 @@ import {M3DLoaderModule} from '../shared/components/m3d/m3d-loader-module';
 
 import {MaterialModule} from '../material.module';
 import {AdvancedMediaPlayerModule} from '../shared/components/video/advanced-video-player.module';
-import {SegmentdetailsComponent} from './segmentdetails.component';
 import {SegmentFeaturesComponent} from './segment-features.component';
 import {PipesModule} from '../shared/pipes/pipes.module';
+import {MatTableModule} from '@angular/material/table';
 
 @NgModule({
-  imports: [MaterialModule, FlexLayoutModule, BrowserModule, AppRoutingModule, M3DLoaderModule, AdvancedMediaPlayerModule, PipesModule],
+  imports: [MaterialModule, FlexLayoutModule, BrowserModule, AppRoutingModule, M3DLoaderModule, AdvancedMediaPlayerModule, PipesModule, MatTableModule],
   declarations: [SegmentFeaturesComponent],
   exports: [SegmentFeaturesComponent],
 })
