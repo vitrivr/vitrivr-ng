f4ba9fe217a95dd440eaab0f682d1e7c71c9755f
Silvan Heller <silvan.heller@unibas.ch>
2022-06-21 16:51:42 +0200

    adding task name

--- src/app/toolbar/dres-timer.component.ts
+++ src/app/toolbar/dres-timer.component.ts
@@ -3,13 +3,14 @@ import {Config} from "../shared/model/config/config.model";
 import {AppConfig} from "../app.config";
 import {DresService} from "../core/basics/dres.service";
 import {skip, tap} from "rxjs/operators";
+import {ClientTaskInfo} from "../../../openapi/dres";
 
 @Component({
   selector: 'app-dres-timer',
   template: `
     <div class="mat-body-2" *ngIf="initialized && config|GetConfigVariablePipe:competitionHost">
       <ng-container *ngIf="countDownFun === null">no active task</ng-container>
-      <ng-container *ngIf="countDownFun">{{timeStr}} remaining</ng-container>
+      <ng-container *ngIf="countDownFun">{{activeTask.name}}: {{timeStr}} remaining</ng-container>
     </div>
     <div class="mat-body-2" *ngIf="!initialized && config|GetConfigVariablePipe:competitionHost">
       loading task information
@@ -23,6 +24,7 @@ export class DresTimerComponent implements OnInit {
   taskSecondsRemaining = 0
   countDownFun = null
   initialized = false
+  activeTask: ClientTaskInfo = null
 
   competitionHost = ((c: Config) => c._config.competition.host)
   timeStr: string;
@@ -40,6 +42,7 @@ export class DresTimerComponent implements OnInit {
         tap(task => {
           if (task) {
             if (task.running) {
+              this.activeTask = task
               clearInterval(this.countDownFun)
               this.taskSecondsRemaining = task.remainingTime
               this.countDownFun = setInterval(() => this.tic(), 1000)
